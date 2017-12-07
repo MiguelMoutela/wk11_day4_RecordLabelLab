3ddced3935fe717fc55ad1b4cbaac92c832bd6b3
Miguel <miguelmoutela@hotmail.com>
2017-12-07 14:32:11 +0000

    record has attributes

---
+++ Specs/Record_spec.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const Record = require('../Record.js');
+
+describe ('Record', function (){
+  let record;
+  beforeEach (function() {
+    record = new Record('artist','title','genre','price');
+  });
+
+  it('should have an artist', function() {
+    assert.strictEqual(record.artist, 'artist')
+  });
+  it('should have an title', function() {
+    assert.strictEqual(record.title, 'title')
+  });
+  it('should have an genre', function() {
+    assert.strictEqual(record.genre, 'genre')
+  });
+  it('should have an price', function() {
+    assert.strictEqual(record.price, 'price')
+  });
+});
